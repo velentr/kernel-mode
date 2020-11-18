e073372bd8377707fd1609fea5a782874e326b5b
Brian Kubisiak <brian@kubisiak.com>
2020-11-17 20:33:40 -0800

    kernel-mode: add minor mode for kernel development
    Add a few syntactic conventions and some configuration for helping with
    flycheck syntax checking.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- kernel.el
+++ kernel.el
@@ -11,6 +11,26 @@
 
 ;;; Code:
 
+(defvar kernel-source-tree (expand-file-name "~/src/linux")
+  "Path to the source tree for kernel headers.")
+
+(define-minor-mode kernel-mode
+  "Minor mode for Linux kernel development."
+  :init-value nil
+  :lighter " Kernel"
+  :keymap nil
+  (cond
+   (kernel-mode
+    (c-set-style "linux")
+    (setq tab-width 8)
+    (setq indent-tabs-mode t)
+    (custom-set-variables
+     '(flycheck-gcc-include-path
+       `(,(concat kernel-source-tree "/include")))))
+   (t
+    (custom-set-variables
+     '(flycheck-gcc-include-path '())))))
+
 (provide 'kernel)
 
 ;;; kernel.el ends here
