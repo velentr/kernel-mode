27c884ece5f708a53e697bec0c9bb6c80621fcd2
Brian Kubisiak <brian@kubisiak.com>
2020-11-17 20:26:31 -0800

    kernel: add initial code template
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

---
+++ kernel.el
@@ -0,0 +1,16 @@
+;;; kernel.el --- Minor mode for Linux kernel development
+
+;; Copyright (C) 2020 Brian Kubisiak <brian@kubisiak.com>
+;;
+;; Author: Brian Kubisiak <brian@kubisiak.com>
+;; URL: http://github.com/velentr/kernel-mode
+
+;;; Commentary:
+
+;; Minor mode for Linux kernel development.
+
+;;; Code:
+
+(provide 'kernel)
+
+;;; kernel.el ends here
