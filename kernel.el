b75e73bc76feb68ae48ce2562488e4581d049435
Brian Kubisiak <brian@kubisiak.com>
2020-11-18 16:30:06 +0000

    kernel: flycheck parameters
    Add include paths, definitions, and includes used by the kernel
    build for flycheck to work correctly on kernel code. This
    requires setting the kernel-arch variable as well as pointing the
    minor mode at a built kernel tree so it can find generated
    headers.
    
    It's still missing a few options and needs some additional
    customization, but should work for the majority of kernel code.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- kernel.el
+++ kernel.el
@@ -14,6 +14,16 @@
 (defvar kernel-source-tree (expand-file-name "~/src/linux")
   "Path to the source tree for kernel headers.")
 
+(defvar kernel-build-dir (expand-file-name "~/src/linux")
+  "Path to the build outputs for the kernel tree.
+
+This path should typically be the path that is passed with O= to the
+kernel build.  It will be used for finding generated header files when
+checking syntax.")
+
+(defvar kernel-arch "x86"
+  "Value of the ARCH= variable passed to the kernel build.")
+
 (define-minor-mode kernel-mode
   "Minor mode for Linux kernel development."
   :init-value nil
@@ -25,11 +35,38 @@
     (setq tab-width 8)
     (setq indent-tabs-mode t)
     (custom-set-variables
+     ;; The kernel build also uses -nostdinc -isystem
+     ;; /usr/lib/gcc-cross/aarch64-linux-gnu/5/include, but I'm not
+     ;; sure how to make that portable yet. Also, setting
+     ;; CROSS_COMPILE= may be necessary for some parts of the kernel.
+     '(flycheck-gcc-language-standard "gnu89")
+     '(flycheck-gcc-includes
+       `(,(concat kernel-source-tree "/include/linux/kconfig.h")))
+     '(flycheck-gcc-definitions '("__KERNEL__"))
+     '(flycheck-gcc-warnings
+       '("all"
+	 "undef"
+	 "strict-prototypes"
+	 "no-trigraphs"
+	 "error-implicit-function-declaration"
+	 "no-format-security"
+	 "frame-larger-than=2048"
+	 "no-unused-but-set-variable"
+	 "declaration-after-statement"
+	 "no-pointer-sign"
+	 "error=implicit-int"
+	 "error=strict-prototypes"
+	 "error=date-time"
+	 "error=incompatible-pointer-types"))
      '(flycheck-gcc-include-path
-       `(,(concat kernel-source-tree "/include")))))
-   (t
-    (custom-set-variables
-     '(flycheck-gcc-include-path '())))))
+       `(,(concat kernel-source-tree "/arch/" kernel-arch "/include")
+	 ,(concat kernel-build-dir "/arch/" kernel-arch "/include/generated/uapi")
+	 ,(concat kernel-build-dir "/arch/" kernel-arch "/include/generated")
+	 ,(concat kernel-source-tree "/include")
+	 ,(concat kernel-build-dir "/include")
+	 ,(concat kernel-source-tree "/arch/" kernel-arch "/include/uapi")
+	 ,(concat kernel-source-tree "/include/uapi")
+	 ,(concat kernel-build-dir "/include/generated/uapi")))))))
 
 (provide 'kernel)
 
